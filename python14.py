65a297ef93b5b3e8c9d10c018a322e054b1cbeaa
Tharathaep <tharathaep3@gmail.com>
2024-08-25 16:08:26 +0700

    Datetime,Math,JSON,Try Except,String Formatting,Input,PIP

---
+++ python14.py
@@ -0,0 +1,15 @@
+"""
+#
+Part: PIP (Python Install Package)
+#
+"""
+import camelcase
+camel = camelcase.CamelCase()
+txt = "hello world my friend"
+print(camel.hump(txt))
+
+# pip list
+# pip install camelcase
+# pip uninstall camelcase
+# python.exe -m pip list
+# python.exe -m pip install camelcase
